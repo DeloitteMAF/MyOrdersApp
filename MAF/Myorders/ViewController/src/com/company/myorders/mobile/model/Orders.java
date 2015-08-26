6342faa47b9377caaa133bc2fb91fcc1b6cbbc19
kauspatel <koustubhpatel@gmail.com>
2015-08-27 02:22:41 +0530

    Added Styling in Order Page

--- MAF/Myorders/ViewController/src/com/company/myorders/mobile/model/Orders.java
+++ MAF/Myorders/ViewController/src/com/company/myorders/mobile/model/Orders.java
@@ -1,5 +1,7 @@
 package com.company.myorders.mobile.model;
 
+import com.company.myorders.mobile.model.utility.OrdersUtilityBean;
+
 import oracle.ateam.sample.mobile.v2.persistence.model.Entity;
 
 import java.math.BigDecimal;
@@ -50,7 +52,8 @@ public class Orders extends Entity {
     }
 
     public String getOrderedDate() {
-        return this.orderedDate;
+        OrdersUtilityBean ordersUtilityBean=new OrdersUtilityBean();
+        return ordersUtilityBean.toDateTime(this.orderedDate);
     }
 
     public void setOrderedDate(String orderedDate) {
